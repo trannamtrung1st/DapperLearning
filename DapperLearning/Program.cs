ec484595b4b7f81491c5c83b3d558590c22f68db
trannamtrung1st <trannamtrung1st@gmail.com>
2023-04-01 18:20:07 +0700

    Add project files.

---
+++ DapperLearning/Program.cs
@@ -0,0 +1,14 @@
+﻿// Connect to the database 
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+var connectionString = "";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    // Create a query that retrieves all books with an author name of "John Smith"    
+    var sql = "SELECT * FROM Books WHERE Author = @authorName";
+
+    // Use the Query method to execute the query and return a list of objects    
+    var books = connection.Query<object>(sql, new { authorName = "John Smith" }).ToList();
+}
